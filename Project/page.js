a201db923692508537e22068307758e1195c2bc3
Shivraj Singh <shivrajsingh160801@gmail.com>
2023-02-11 22:33:27 +0530

    Shivraj--Page 2 Done

---
+++ Project/page.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+
+    // Shivraj Page 2 JS
+
+    var part2div = $(".part2div");
+
+    part2div.click(function () {
+      part2div.css("background-color", "lightyellow");
+    });
+
+    part2div.mouseleave(function () {
+      part2div.css("background-color", "white");
+    });
+
+    var part3div = $(".part3div");
+
+    part3div.click(function () {
+      part3div.css("background-color", "lightyellow");
+    });
+
+    part3div.mouseleave(function () {
+      part3div.css("background-color", "white");
+    });
+
+    
+    var part4div = $(".part4div");
+
+    part4div.click(function () {
+      part4div.css("background-color", "lightyellow");
+    });
+
+    part4div.mouseleave(function () {
+      part4div.css("background-color", "white");
+    });
+
+     
+      const div_17_c2 = $(".div_17_c2");
+      const hovertext17c2 = $(".hover-text_17_c2");
+
+      div_17_c2.mouseenter(function () {
+        hoverText.show();
+      });
+
+      div_17_c2.mouseleave(function () {
+        hoverText.hide();
+      });
+
+// --------------------------Page 2 Over-------------------------------------------
+  });
