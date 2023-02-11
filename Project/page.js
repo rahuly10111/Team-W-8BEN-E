827e0da72316da6de8b47401121b0e1ba1037df6
Shivraj Singh <shivrajsingh160801@gmail.com>
2023-02-11 23:07:38 +0530

    Page 3 Done -- By Soham

--- Project/page.js
+++ Project/page.js
@@ -46,4 +46,198 @@ $(document).ready(function () {
       });
 
 // --------------------------Page 2 Over-------------------------------------------
+
+// --------------------------Page 3 End-------------------------------------------
+ 
+            //  checkbox for 5
+             // checkbox for 33
+             const checkbox5 = $("#checkbox5");
+            const checkbocdiv5 = $(".checkbocdiv5");
+            const hoverText5 = $(".hover-text5");
+
+            checkbox5.mouseenter(function () {
+                hoverText5.show();
+            });
+
+            checkbocdiv5.mouseleave(function () {
+                hoverText5.hide();
+            });
+            const partdiv5 = $("#partdiv5");
+
+            partdiv5.click(function () {
+                partdiv5.css("background-color", "lightyellow");
+            });
+
+            partdiv5.mouseleave(function () {
+                partdiv5.css("background-color", "white");
+            });
+            
+            // 6tstart
+            const checkbox6 = $("#checkbox6");
+            const checkbocdiv6 = $(".checkbocdiv6");
+            const hoverText6 = $(".hover-text6");
+
+            checkbox6.mouseenter(function () {
+                hoverText6.show();
+            });
+
+            checkbocdiv6.mouseleave(function () {
+                hoverText6.hide();
+            });
+            const partdiv6 = $("#partdiv6");
+
+            partdiv6.click(function () {
+                partdiv6.css("background-color", "lightyellow");
+            });
+
+            partdiv6.mouseleave(function () {
+                partdiv6.css("background-color", "white");
+            });
+            // 6th end
+            //  checkbox for 7 start
+            const checkbox7 = $("#checkbox7");
+            const checkbocdiv7 = $(".checkbocdiv7");
+            const hoverText7 = $(".hover-text7");
+
+            checkbox7.mouseenter(function () {
+                hoverText7.show();
+            });
+
+            checkbocdiv7.mouseleave(function () {
+                hoverText7.hide();
+            });
+            const partdiv7 = $("#partdiv7");
+
+            partdiv7.click(function () {
+                partdiv7.css("background-color", "lightyellow");
+            });
+
+            partdiv7.mouseleave(function () {
+                partdiv7.css("background-color", "white");
+            });
+
+            // checkbox7 end
+            // checkbox8 start here
+            const checkbox8 = $("#checkbox8");
+            const checkbocdiv8 = $(".checkbocdiv8");
+            const hoverText8 = $(".hover-text8");
+
+            checkbox8.mouseenter(function () {
+                hoverText8.show();
+            });
+
+            checkbocdiv8.mouseleave(function () {
+                hoverText8.hide();
+            });
+            const partdiv8 = $("#partdiv8");
+
+            partdiv8.click(function () {
+                partdiv8.css("background-color", "lightyellow");
+            });
+
+            partdiv8.mouseleave(function () {
+                partdiv8.css("background-color", "white");
+            });
+
+
+            // checkbox8 end here
+             // checkbox9 start here
+             const checkbox9 = $("#checkbox9");
+            const checkbocdiv9 = $(".checkbocdiv9");
+            const hoverText9 = $(".hover-text9");
+
+            checkbox9.mouseenter(function () {
+                hoverText9.show();
+            });
+
+            checkbocdiv9.mouseleave(function () {
+                hoverText9.hide();
+            });
+            const partdiv9 = $("#partdiv9");
+
+            partdiv9.click(function () {
+                partdiv9.css("background-color", "lightyellow");
+            });
+
+            partdiv9.mouseleave(function () {
+                partdiv9.css("background-color", "white");
+            });
+
+
+            // checkbox9 end here
+
+            // checkbox10 start here
+             // checkbox10 start here
+             const checkbox10 = $("#checkbox10");
+            const checkbocdiv10 = $(".checkbocdiv10");
+            const hoverText10 = $(".hover-text10");
+
+            checkbox10.mouseenter(function () {
+                hoverText10.show();
+            });
+
+            checkbocdiv10.mouseleave(function () {
+                hoverText10.hide();
+            });
+            const partdiv10 = $("#partdiv10");
+
+            partdiv10.click(function () {
+                partdiv10.css("background-color", "lightyellow");
+            });
+
+            partdiv10.mouseleave(function () {
+                partdiv10.css("background-color", "white");
+            });
+
+
+            // checkbox10 end here
+           // checkbox10b start
+           const checkbox10b = $("#checkbox10b");
+            const checkbocdiv10b = $(".checkbocdiv10b");
+            const hoverText10b = $(".hover-text10b");
+
+            checkbox10b.mouseenter(function () {
+                hoverText10b.show();
+            });
+
+            checkbocdiv10b.mouseleave(function () {
+                hoverText10b.hide();
+            });
+            // checkbox10b end
+
+            // checkbox10c start
+            const checkbox10c = $("#checkbox10c");
+            const checkbocdiv10c = $(".checkbocdiv10c");
+            const hoverText10c = $(".hover-text10c");
+
+            checkbox10c.mouseenter(function () {
+                hoverText10c.show();
+            });
+
+            checkbocdiv10c.mouseleave(function () {
+                hoverText10c.hide();
+            });
+            
+
+            // checkbox10c end
+
+            // checkbox10d start
+            // const checkbox10d = $("#checkbox10d");
+            // const checkbocdiv10d = $(".checkbocdiv10d");
+            // const hoverText10d = $(".hover-text10d");
+
+            // checkbox10d.mouseenter(function () {
+            //     hoverText10d.show();
+            // });
+
+            // checkbocdiv10d.mouseleave(function () {
+            //     hoverText10d.hide();
+            // });
+            
+
+            // checkbox10d end
+            
+// --------------------------Page 3 Over-------------------------------------------
+
+
   });
