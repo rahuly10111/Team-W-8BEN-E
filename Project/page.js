348e1c0c66250888d624a2586d01060d2a2c3052
Samiksha Mandloi <samiksha140302@gmail.com>
2023-02-13 09:18:28 +0530

    samiksha page 6 done

--- Project/page.js
+++ Project/page.js
@@ -245,7 +245,7 @@ $(document).ready(function () {
     const checkbocdiv25a = $(".checkbocdiv25a");
     const hoverText25a = $(".hover-text25a");
 
-    checkbox35.mouseenter(function () {
+    checkbox25.mouseenter(function () {
         hoverText25a.show();
     });
 
